cf191a54076a940cd6a9d7f52aabb646298fba2f
rubythalib33 <rubyabdullah14@gmail.com>
2023-03-13 10:21:33 +0700

    update

--- server/server.h
+++ server/server.h
@@ -9,10 +9,42 @@ public:
     Session(tcp::socket socket, StoreData& store) : socket_(std::move(socket)), store_(store) {}
 
     void start() {
-        doRead();
+        authenticate();
     }
 
 private:
+    void authenticate(){
+        auto self(shared_from_this());
+        boost::asio::async_read_until(socket_, buffer_, '\n',
+            [this, self](boost::system::error_code ec, std::size_t length) {
+                if (!ec) {
+                    std::string data(boost::asio::buffer_cast<const char*>(buffer_.data()), length);
+                    buffer_.consume(length);
+                    std::istringstream iss(data);
+                    std::string method, username, password;
+                    iss >> method >> username >> password;
+                    std::string expected_username = std::getenv("USERNAME") != nullptr ? std::getenv("USERNAME") : "root";
+                    std::string expected_password = std::getenv("PASSWORD") != nullptr ? std::getenv("PASSWORD") : "root";
+                    if (username == expected_username && password == expected_password) {
+                        generateToken();
+                        doWrite(generated_tokens_.begin()->c_str());
+                        doRead();
+                    } else {
+                        doWrite("ERROR: Authentication failed\n");
+                        socket_.close();
+                    }
+                } else if (ec != boost::asio::error::eof) {
+                    std::cerr << "Error: " << ec.message() << std::endl;
+                }
+            });
+    }
+
+    void generateToken() {
+        token_ = "token_" + std::to_string(std::chrono::system_clock::now().time_since_epoch().count());
+        generated_tokens_.insert(token_);
+        std::cout << "Generated token: " << token_ << std::endl;
+    }
+
     void doRead() {
         auto self(shared_from_this());
         boost::asio::async_read_until(socket_, buffer_, '\n',
@@ -21,59 +53,86 @@ private:
                     std::string data(boost::asio::buffer_cast<const char*>(buffer_.data()), length);
                     buffer_.consume(length);
                     std::istringstream iss(data);
-                    std::string method;
-                    iss >> method;
+                    std::string method, tokens;
+                    iss >> method >> tokens;
                     if (method == "CREATE") {
-                        std::string label;
-                        std::vector<double> embedding;
-                        iss >> label;
-                        double val;
-                        while (iss >> val) {
-                            embedding.push_back(val);
+                        if (generated_tokens_.find(tokens) != generated_tokens_.end()) {
+                            generated_tokens_.erase(tokens);
+                            std::string label;
+                            std::vector<double> embedding;
+                            iss >> label;
+                            double val;
+                            while (iss >> val) {
+                                embedding.push_back(val);
+                            }
+                            store_.insertData(label, embedding);
+                            doWrite("OK\n");
+                        } else {
+                            doWrite("ERROR: Invalid token\n");
                         }
-                        store_.insertData(label, embedding);
-                        doWrite("OK\n");
+                        
                     } else if (method == "READ") {
-                        std::string label;
-                        iss >> label;
-                        std::vector<double> embedding = store_.readData(label);
-                        std::ostringstream oss;
-                        for (double val : embedding) {
-                            oss << val << " ";
+                        if (generated_tokens_.find(tokens) != generated_tokens_.end()) {
+                            std::string label;
+                            iss >> label;
+                            std::vector<double> embedding = store_.readData(label);
+                            std::ostringstream oss;
+                            for (double val : embedding) {
+                                oss << val << " ";
+                            }
+                            oss << "\n";
+                            doWrite(oss.str());
+                        } else {
+                            doWrite("ERROR: Invalid token\n");
                         }
-                        oss << "\n";
-                        doWrite(oss.str());
                     } else if (method == "UPDATE") {
-                        std::string label;
-                        std::vector<double> embedding;
-                        iss >> label;
-                        double val;
-                        while (iss >> val) {
-                            embedding.push_back(val);
+                        if (generated_tokens_.find(tokens) != generated_tokens_.end()) {
+                            std::string label;
+                            std::vector<double> embedding;
+                            iss >> label;
+                            double val;
+                            while (iss >> val) {
+                                embedding.push_back(val);
+                            }
+                            store_.updateData(label, embedding);
+                            doWrite("OK\n");
+                        } else {
+                            doWrite("ERROR: Invalid token\n");
                         }
-                        store_.updateData(label, embedding);
-                        doWrite("OK\n");
                     } else if (method == "DELETE") {
-                        std::string label;
-                        iss >> label;
-                        store_.deleteData(label);
-                        doWrite("OK\n");
+                        if (generated_tokens_.find(tokens) != generated_tokens_.end()) {
+                            std::string label;
+                            iss >> label;
+                            store_.deleteData(label);
+                            doWrite("OK\n");
+                        } else {
+                            doWrite("ERROR: Invalid token\n");
+                        }
                     } else if (method == "SEARCH") {
+                        if (generated_tokens_.find(tokens) != generated_tokens_.end()) {
+                            std::vector<double> query;
+                            double val;
+                            while (iss >> val) {
+                                query.push_back(val);
+                            }
+                            double threshold;
+                            int limit;
+                            iss >> threshold >> limit;
+                            std::vector<std::string> similar = store_.search(query, threshold, limit);
+                            std::ostringstream oss;
+                            for (std::string label : similar) {
+                                oss << label << " ";
+                            }
+                            oss << "\n";
+                            doWrite(oss.str());
+                        } else {
+                            doWrite("ERROR: Invalid token\n");
+                        }
                         std::vector<double> query;
                         double val;
                         while (iss >> val) {
                             query.push_back(val);
                         }
-                        double threshold;
-                        int limit;
-                        iss >> threshold >> limit;
-                        std::vector<std::string> similar = store_.search(query, threshold, limit);
-                        std::ostringstream oss;
-                        for (std::string label : similar) {
-                            oss << label << " ";
-                        }
-                        oss << "\n";
-                        doWrite(oss.str());
                     } else {
                         doWrite("ERROR: Invalid method\n");
                     }
