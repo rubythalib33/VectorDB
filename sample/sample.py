2907d5a506b8ced3b43b7d935840234bad34eddd
rubythalib33 <rubyabdullah14@gmail.com>
2023-05-10 20:34:48 +0700

    update

--- sample/sample.py
+++ sample/sample.py
@@ -54,4 +54,21 @@ if __name__ == '__main__':
     response = client.send_request(f'DELETE {token} label\n')
     print(response)
 
+    # Create 100 data
+    import random
+    for i in range(100):
+        response = client.send_request(f'CREATE {token} label_{i} {random.random()} {random.random()} {random.random()}\n')
+        print(response)
+
+    # create data
+    response = client.send_request(f'CREATE {token} label 1.0 2.0 3.0\n')
+    
+    # make cluster of 5
+    response = client.send_request(f'MAKE_CLUSTER {token} 5\n')
+    print(response)
+
+    # search cluster
+    response = client.send_request(f'SEARCH_CLUSTER {token} 1.0 2.0 3.0 0.5 1\n')
+    print(response)
+
     client.close()
